5cc620d6f2f4b2f69525ab4263c330fe941a3fe2
ArshiaPazoki <a.pazoki@asax.local>
2022-08-15 13:28:49 +0430

    C# Introduction

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// A tour of the C# language
+
+// C# (pronounced "See Sharp") is a modern, object-oriented, and type-safe programming language.
+// C# is an object-oriented, component-oriented programming language.
+// At its core, C# is an object-oriented language. You define types and their behavior.
+
+// Several C# features help create robust and durable applications:
+//      1 - Garbage collection automatically reclaims memory occupied by unreachable unused objects.
+//      2 - Nullable types guard against variables that don't refer to allocated objects.
+//      3 - Exception handling provides a structured and extensible approach to error detection and recovery.
+//      4 - Lambda expressions support functional programming techniques.
+//      5 - Language Integrated Query (LINQ) syntax creates a common pattern for working with data from any source.
+//      6 - Language support for asynchronous operations provides syntax for building distributed systems.
+//      7 - C# has a unified type system. All C# types, including primitive types such as int and double, inherit from a single root object type.
+//          All types share a set of common operations. Values of any type can be stored, transported, and operated upon in a consistent manner.
+//          C# supports both user-defined reference types and value types.
+//      8 - C# allows dynamic allocation of objects and in-line storage of lightweight structures.
+//      9 - C# supports generic methods and types, which provide increased type safety and performance.
+//      10- C# provides iterators, which enable implementers of collection classes to define custom behaviors for client code.
+//      11- C# emphasizes versioning to ensure programs and libraries can evolve over time in a compatible manner.
+//          Aspects of C#'s design that were directly influenced by versioning considerations include the separate virtual and override modifiers,
+//          the rules for method overload resolution, and support for explicit interface member declarations.
+
+// .NET architecture
